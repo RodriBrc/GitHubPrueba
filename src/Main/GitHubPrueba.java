4e19e76dec878c6406b011192fe0fde5b6b0bd24
Rodri Bracco <bracco.rodri@gmail.com>
2021-08-25 20:40:36 -0300

    Pimer Commit

---
+++ src/Main/GitHubPrueba.java
@@ -0,0 +1,13 @@
+
+
+package Main;
+
+
+public class GitHubPrueba {
+
+    
+    public static void main(String[] args) {
+       
+    }
+
+}
---
+++ src/entidades/github.java
@@ -0,0 +1,58 @@
+package entidades;
+
+public class github {
+    
+    
+    private String name;
+    private String adress;
+    private Integer dni;
+    private Integer age;
+
+    public github() {
+    }
+
+    public github(String name, String adress, Integer dni, Integer age) {
+        this.name = name;
+        this.adress = adress;
+        this.dni = dni;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public Integer getDni() {
+        return dni;
+    }
+
+    public void setDni(Integer dni) {
+        this.dni = dni;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "github{" + "name=" + name + ", adress=" + adress + ", dni=" + dni + ", age=" + age + '}';
+    }
+      
+}
